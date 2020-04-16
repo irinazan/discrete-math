fd759884473bbf08d8665d41c5a886dcc7e8236e
Pawel Jan <speciallyfororigin@gmail.com>
2020-04-16 16:43:24 +0300

    whooops

--- lab2.cpp
+++ lab2.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 void assignment (vector<int>& a, vector<int>& b);
 
-void preprocess (vector<int>& a, vector<int>& b, vector<int>& cache_a, vector<int>& cache_b, bool& intersection_exists);
+void preprocess (vector<int>& a, vector<int>& b, vector<int>& cache_a, vector<int>& cache_b, bool& intersection_exists);    //TODO:ответь в телеграме
 
 int menu (vector<int>& a, vector<int>& b, vector<int>& cache_a, vector<int>& cache_b, bool& intersection_exists);
 
@@ -110,21 +110,18 @@ int menu (vector<int>& a, vector<int>& b, vector<int>& cache_a, vector<int>& cac
     }
 }
 
-void intersection (vector<int>& a, vector<int>& cache_a, bool& intersection_exists) {
+void intersection (vector<int>& a, vector<int>& cash_a, bool& intersection_exists) {
     if (intersection_exists) {
         vector<int> r;
-        r = a;
-        for (int i = 0; i < r.size(); ++i) {
-            if (i == cache_a[i]) {
-                continue;
-            }
-            r.erase(r.begin() + i);
+        r.reserve(cash_a.size());   //preallocate memory
+        for (int i : cash_a) {  //for (int i = 0; i < cash_a.size(); ++i)
+            r.push_back(a[i]);
         }
         cout << "Пересечение множеств: {";
         for (int i = 0; i < r.size(); ++i) {
             cout << r[i];
             if (i != r.size() - 1) {
-                cout << "," << " ";
+                cout << ",";
             } else {
                 cout << "}" << endl;
             }
@@ -135,12 +132,12 @@ void intersection (vector<int>& a, vector<int>& cache_a, bool& intersection_exis
     }
 }
 
-void _union (vector<int>& a, vector<int>& b, vector<int>& cache_a, bool& intersection_exists){
+void _union (vector<int>& a, vector<int>& b, vector<int>& cash_a, bool& intersection_exists) {
     vector<int> r;
     r = a;
     if (intersection_exists) {
-        for (int i : cache_a) {      //for (int i = 0; i < cash_a.size(); ++i)
-            r.erase(r.begin() + i);
+        for (int i = cash_a.size(); i > 0; --i) {   //for (int i = 0; i < cash_a.size(); ++i)
+            r.erase(r.begin() + (cash_a[i-1]));
         }
     }
     r.insert(r.end(), b.begin(), b.end());
@@ -156,12 +153,12 @@ void _union (vector<int>& a, vector<int>& b, vector<int>& cache_a, bool& interse
     }
 }
 
-void set_difference (vector<int>& a, vector<int>& b, vector<int>& cache_a, vector<int>& cache_b, bool& intersection_exists) {
+void set_difference (vector<int>& a, vector<int>& b, vector<int>& cash_a, vector<int>& cash_b, bool& intersection_exists) {
     vector<int> r;
     r = a;
     if (intersection_exists) {
-        for (int i : cache_a) {      //for (int i = 0; i < cash_a.size(); ++i)
-            r.erase(r.begin() + i);
+        for (int i = cash_a.size(); i > 0; --i) {   //for (int i = 0; i < cash_a.size(); ++i)
+            r.erase(r.begin() + (cash_a[i-1]));
         }
     }
     cout << "Разность множеств\n";
@@ -182,8 +179,8 @@ void set_difference (vector<int>& a, vector<int>& b, vector<int>& cache_a, vecto
     }
     r = b;
     if (intersection_exists) {
-        for (int i : cache_b) {      //for (int i = 0; i < cash_b.size(); ++i)
-            r.erase(r.begin() + i);
+        for (int i = cash_b.size(); i > 0; --i) {   //for (int i = 0; i < cash_a.size(); ++i)
+            r.erase(r.begin() + (cash_b[i-1]));
         }
     }
     cout << "B\\A:";
@@ -215,7 +212,7 @@ void cartesian_product (vector<int>& a, vector<int>& b) // Декартово п
             if ( (i == a.size()-1) && (j == b.size() - 1)){
                 cout << "(" << a[i] << "," << b[j] << ")"; break;
             }
-            cout << "(" << a[i] << "," << b[j] << "), ";
+            cout << "(" << a[i] << "," << b[j] << "), ";    //TODO:поменяй здесь обычные скобки на фигурные
 
         }
     }
